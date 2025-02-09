1fbaeafe716f05d7bd0e9934daa513864b51ef0b
Tashcan <git@tashcan.com>
2025-02-08 17:04:25 -0800

    Ooops

--- xmake-packages/packages/s/spud/xmake.lua
+++ xmake-packages/packages/s/spud/xmake.lua
@@ -3,7 +3,6 @@ add_deps("cmake")
 set_sourcedir(path.join(os.scriptdir(), "spud-src"))
 
 on_install(function(package)
-    print(package)
     local configs = {}
     table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
     table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
