d912611fa1eca49fc54f363bdf8377dfebf8def0
Mark Brugnoli-Vinten <netniv@hotmail.com>
2026-07-19 16:03:45 +0100

    Update loading screen and version number

--- mods/src/version.h
+++ mods/src/version.h
@@ -3,14 +3,14 @@
 // clang-format off
 #define VERSION_MAJOR               1
 #define VERSION_MINOR               1
-#define VERSION_REVISION            2
-#define VERSION_PATCH               2
+#define VERSION_REVISION            4
+#define VERSION_PATCH               0
 
 #define STRINGIFY_(s)               #s
 #define STRINGIFY(s)                STRINGIFY_(s)
 
 #if VERSION_PATCH
-	#define VERSION_PATCH_STR " (CX-Beta)"
+	#define VERSION_PATCH_STR " (Dev)"
 #else
 	#define VERSION_PATCH_STR ""
 #endif
